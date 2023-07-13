089d39f50c01d603d18429e6f4c8c29ea7b7f091
insiya <insiyahusain3@gmail.com>
2023-07-13 21:09:01 +0530

    Initial commit

---
+++ javatp.js
@@ -0,0 +1,12 @@
+
+// Set quote and author
+var quoteText = "Dream, dream, dream. Dreams transform into thoughts and thoughts result in action.";
+var quoteAuthor = "Dr. APJ Abdul Kalam";
+document.getElementById("quote-text").innerText = '"' + quoteText + '"';
+document.getElementById("quote-author").innerText = "- " + quoteAuthor;
+
+// Set life and contribution information
+var lifeInfo = "Avul Pakir Jainulabdeen Abdul Kalam, also known as Dr. APJ Abdul Kalam, was an Indian scientist, professor, and statesman. He was born on October 15, 1931, in Rameswaram, Tamil Nadu, India.";
+var contributionInfo = "Dr. Kalam played a crucial role in the development of India's space and missile programs. He served as the 11th President of India from 2002 to 2007 and was widely regarded as the 'People's President' due to his down-to-earth nature and dedication to the welfare of the nation. Throughout his life, Dr. Kalam emphasized the importance of education and inspired millions of students with his speeches and writings. He believed in the power of youth and encouraged them to dream big and work hard to achieve their goals.";
+document.getElementById("life-info").innerText = lifeInfo;
+document.getElementById("contribution-info").innerText = contributionInfo;
